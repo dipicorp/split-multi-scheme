83c5a32593aba8eb87bc3efd9206bab2f6dd6738
phat-pham-go2joy <phat.pham@go2joy.vn>
2023-01-06 17:35:47 +0700

    Init proj

---
+++ MultiScheme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiScheme
+//
+//  Created by phat nho on 06/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var centerlb: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
